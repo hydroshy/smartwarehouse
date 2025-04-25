b0715f0f37c36e9a94bf1ab412c4b71578ec4280
hydroshy <phanloi1403@gmail.com>
2025-04-25 23:13:33 +0700

    newupdated

--- app/src/main/java/com/example/smartwarehouse/ChangePasswordActivity.java
+++ app/src/main/java/com/example/smartwarehouse/ChangePasswordActivity.java
@@ -1,8 +1,5 @@
 package com.example.smartwarehouse;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
